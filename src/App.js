38cb08c771a9985717c38670bde445a21ca417ad
Jean Ricardo Monteiro Rodrigues <Jeanrmr@hotmail.com>
2019-12-02 18:04:46 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Card from './shared/components/card/card'
 import logo from './assets/logo.png'
+import Axios from 'axios'
+
+function renderCard({ name, img, level }) {
+  return (
+    <Card>
+      <div style={{ flexDirection: 'row', display: 'flex', margin: '10px' }}>
+
+        <div style={{ width: '40%', margin: 'auto' }}>
+          <img src={img} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
+        </div>
+
+        <div style={{ width: '60%' }}>
+          <h3>{name}</h3>
+          <p>{level}</p>
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <div style={{ width: '25%', margin: 'auto'}}>
-        <img src={logo} alt="Logo" style={{maxWidth: '100%', height: 'auto'}} />
+  const [digimons, setDigimons] = useState([]);
+
+  const renderList = () => {
+    return digimons.map((digimon) =>
+      <div>
+        {renderCard(digimon)}
       </div>
-      <Card>
-        <p>HelloWorld</p>
-      </Card>
+    )
+  }
+
+  const callApi = async () => {
+    let { data } = await Axios.get('https://digimon-api.herokuapp.com/api/digimon')
+    setDigimons(data)
+  }
+
+  callApi()
+
+  return (
+    <div>
+      <div style={{ width: '20%', margin: 'auto' }}>
+        <img src={logo} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%', marginTop: '20px', flexWrap: 'wrap' }}>
+        {renderList()}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/shared/components/card/styles.js
+++ src/shared/components/card/styles.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
 const Box = styled.div`
-    border: 1px solid red;
-    width: 50%;
-    margin: auto;
+    border: 1px solid #eee;
+    -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+    -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+    border-radius: 8px;
+    background-color: #fff;
+    margin: 10px;
 `
 
 export {
