413f7f985ec50ffb42b878f2769c2edfaad03d5d
Jean Ricardo Monteiro Rodrigues <Jeanrmr@hotmail.com>
2019-12-03 11:58:01 -0300

    digimon list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Card from './shared/components/card/card'
 import logo from './assets/logo.png'
 import Axios from 'axios'
+import styled from 'styled-components';
+
+const Card = styled.div`
+    border: 1px solid #eee;
+    -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+    -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.35);
+    border-radius: 8px;
+    background-color: #fff;
+    margin: 10px;
+    width: 300px;
+`
+
+const CardsContainer = styled.div`
+    display: flex; 
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+    margin-top: 20px;
+    flex-wrap: wrap;
+`
 
 function renderCard({ name, img, level }) {
   return (
-    <Card>
-      <div style={{ flexDirection: 'row', display: 'flex', margin: '10px' }}>
+    <Card style={{ flexDirection: 'row', display: 'flex', padding: '10px' }}>
 
-        <div style={{ width: '40%', margin: 'auto' }}>
-          <img src={img} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
-        </div>
+      <div style={{ width: '40%', margin: 'auto' }}>
+        <img src={img} alt="" style={{ maxWidth: '100%', height: 'auto' }} />
+      </div>
 
-        <div style={{ width: '60%' }}>
-          <h3>{name}</h3>
-          <p>{level}</p>
-        </div>
+      <div style={{ width: '60%' }}>
+        <h3>{name}</h3>
+        <p>{level}</p>
       </div>
     </Card>
   )
 }
 
-
-
 function App() {
   const [digimons, setDigimons] = useState([]);
+
+  useEffect(() => {
+    const callApi = async () => {
+      let { data } = await Axios.get('https://digimon-api.herokuapp.com/api/digimon')
+      setDigimons(data)
+    }
+    callApi()
+  });
 
   const renderList = () => {
     return digimons.map((digimon) =>
@@ -35,22 +59,15 @@ function App() {
     )
   }
 
-  const callApi = async () => {
-    let { data } = await Axios.get('https://digimon-api.herokuapp.com/api/digimon')
-    setDigimons(data)
-  }
-
-  callApi()
-
   return (
     <div>
       <div style={{ width: '20%', margin: 'auto' }}>
         <img src={logo} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
       </div>
 
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%', marginTop: '20px', flexWrap: 'wrap' }}>
+      <CardsContainer>
         {renderList()}
-      </div>
+      </CardsContainer>
     </div>
   )
 }
