1df33c82dd0519a90b41b4e5a2606976d2387f93
JeanMonteiro <Jeanrmr@hotmail.com>
2019-12-03 13:26:31 -0300

    Updates

---
+++ precache-manifest.4bfabd33dea9a1bc8971b06fdcf756f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001ee94f215003d2c863656d151c6c2d",
+    "url": "/digimons/index.html"
+  },
+  {
+    "revision": "ba2186da14a46923fa3e",
+    "url": "/digimons/static/css/main.89c2ada7.chunk.css"
+  },
+  {
+    "revision": "290bbeca759709fee810",
+    "url": "/digimons/static/js/2.cb664ee3.chunk.js"
+  },
+  {
+    "revision": "ba2186da14a46923fa3e",
+    "url": "/digimons/static/js/main.7b037a50.chunk.js"
+  },
+  {
+    "revision": "39ad0fe788cd9057f4b7",
+    "url": "/digimons/static/js/runtime-main.215d224f.js"
+  },
+  {
+    "revision": "631c4cfe9d619b033e533be5d650ffd9",
+    "url": "/digimons/static/media/logo.631c4cfe.png"
+  }
+]);
